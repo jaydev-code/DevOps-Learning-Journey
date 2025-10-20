bec9f304cf98db7ce3e3e5349e5b70b3eb08efc4
jaydev-code <jayfelisdev@gmail.com>
2025-10-20 12:40:17 +0800

    feat: add df -h | grep for disk space info of system filesystem

--- 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
+++ 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
@@ -4,5 +4,9 @@
 
 echo "hostname    : $(hostname)"
 echo "uptime      : $(uptime -p )"
-echo "disk usage : $(free -h )"
+echo "$(free -h )"
+echo "----------------------------------------------------------------------------------------"
+echo " $(df -h | grep C )  "
+echo "$( df -h | grep /dev/sdd )"
+echo "$( df -h | grep drivers )"
 
