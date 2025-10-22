db3a97e20e2217764d1391d9c95670ae839671ad
jaydev-code <jayfelisdev@gmail.com>
2025-10-22 22:30:38 +0800

    feat: add color for design

--- 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
+++ 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+NC='\033[0m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+echo -e "${BLUE}===== Memory Status =====${NC}"
 
+echo -e "hostname    : ${BLUE}$(hostname)${NC}"
+echo -e "uptime      : ${RED}$(uptime -p )${NC}"
+echo -e "${BLUE}$(free -h )"
+echo -e "${NC}----------------------------------------------------------------------------------------"
+echo -e "${BLUE}$(df -h | grep C )${NC}  "
+echo -e "${BLUE}$( df -h | grep /dev/sdd )${NC}"
+echo -e "${BLUE}$( df -h | grep drivers )${NC}"
 
-echo "hostname    : $(hostname)"
-echo "uptime      : $(uptime -p )"
-echo "$(free -h )"
-echo "----------------------------------------------------------------------------------------"
-echo " $(df -h | grep C )  "
-echo "$( df -h | grep /dev/sdd )"
-echo "$( df -h | grep drivers )"
 
