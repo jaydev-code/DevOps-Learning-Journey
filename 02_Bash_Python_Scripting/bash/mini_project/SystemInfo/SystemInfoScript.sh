c05804afe6f4653372cf1dd774a30d4fd5f9d647
jaydev-code <jayfelisdev@gmail.com>
2025-10-28 11:45:27 +0800

    feat: add time,date and user details

--- 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
+++ 02_Bash_Python_Scripting/bash/mini_project/SystemInfo/SystemInfoScript.sh
@@ -3,12 +3,16 @@
 NC='\033[0m'
 BLUE='\033[0;34m'
 RED='\033[0;31m'
-echo -e "${BLUE}===== Memory Status =====${NC}"
-
-echo -e "hostname    : ${BLUE}$(hostname)${NC}"
-echo -e "uptime      : ${RED}$(uptime -p )${NC}"
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+echo -e "${BLUE}===== Systen Information ====="
+echo -e "${GREEN}hostname      : ${RED}$(hostname)"
+echo -e "${GREEN}uptime        : ${RED}$(uptime -p )"
+echo -e "${GREEN}User Login    : ${RED}$(whoami)"
+echo -e "${GREEN}date & Time   : ${RED}$(date)"
+echo -e "${BLUE}======  Mermory Status  ======${NC}"
 echo -e "${BLUE}$(free -h )"
-echo -e "${NC}----------------------------------------------------------------------------------------"
+echo -e "${BLUE}====== DIsk Info =======${NC}"
 echo -e "${BLUE}$(df -h | grep C )${NC}  "
 echo -e "${BLUE}$( df -h | grep /dev/sdd )${NC}"
 echo -e "${BLUE}$( df -h | grep drivers )${NC}"
