f7a4029e49d52ad21ae518691edaceb559507c65
jaydev-code <jayfelisdev@gmail.com>
2025-11-13 19:14:06 +0800

    feat : create new automated folder for automated backup script

--- 02_Bash_Python_Scripting/bash/mini_project/AutomatedBackup/automatedBackupScript.sh
+++ 02_Bash_Python_Scripting/bash/mini_project/AutomatedBackup/automatedBackupScript.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-date="$(date +%Y-%m-%d)"
+read -p "enter source directories to archive : " sour 
+read -p "enter directories where to place archive file : " dest
 
+date=$(date +%Y-%m-%d)
+
+tar -czf "$dest/backup-$date.tar.gz" "$sour"
+
+echo "$sour is successfully Archive : "
 
